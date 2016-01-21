1076842537ebdc58bf6e542c3ccf431638cb1c90
Scott Murphy <scott@standardco.de>
2016-01-21 11:36:40 -0500

    added customer response headers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,25 @@
 class HomeController < ApplicationController
-
+  require "net/http"
+  require "uri"
 
   def index
+    puts '!--------@@--------!'
+    puts request.original_url
+    response.headers['Content-Type'] = 'text/xml; charset=utf-8'
+    response.headers['Alternate-Protocol'] = '443:quic,p=1'
+    response.headers['Alternate-Protocol'] = 'quic=":443"; ma=604800; v="30,29,28,27,26,25"'
 
+    puts '!--------@@--------!'
+    #
+    # response = http.request(request)
   end
 
   def forms
+    response.headers['Content-Type'] = 'text/xml; charset=utf-8'
+    response.headers['Alternate-Protocol'] = '443:quic,p=1'
+    response.headers['Alternate-Protocol'] = 'quic=":443"; ma=604800; v="30,29,28,27,26,25"'
 
-
-    # HTTParty.post url, :body => xml, :headers => {'Content-type' => 'text/xml'}
-    render :content_type => 'text/xml'
+    # render :content_type => 'text/xml'
   end
 
 end
